6e03f1fe34ddfd40d9e7938eeae2b6022adcc403
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-04-17 17:28:01 +0300

    created a class for the accounts

--- account.py
+++ account.py
@@ -1,9 +1,18 @@
 class User:
 
-    def __init__(self,first_name,last_name,phone_number,email):
+    user_list = []
+
+    def __init__(self,first_name,last_name,email, password):
 
 
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = number
         self.email = email
+        self.password = password
+
+class Credentials:
+
+    credential_list = []
+
+    def __init__(self, account_name, username, password):
