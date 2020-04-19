7610802b4b2ef6acee1515081b7422cba92a555c
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-04-19 19:41:20 +0300

    created first test

--- account_test.py
+++ account_test.py
@@ -2,4 +2,14 @@ import unittest
 from account import User
 from account import Credentials
 
-class TestAccount (unittest.TestCase)
+class TestAccount (unittest.TestCase):
+
+        def setUp(self):
+        		'''
+        		Function to create a user account before each test
+        		'''
+        		self.new_user = User('Mary','Ng\'ang\'a','pswd100')
+
+
+if __name__ ==  '__main__':
+    unittest.main()
