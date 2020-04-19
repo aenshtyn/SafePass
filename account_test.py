cce500b834414ddc85b4125f9c8dfb54b100172e
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-04-19 19:54:56 +0300

    created test to check if users details are savedt

--- account_test.py
+++ account_test.py
@@ -8,8 +8,20 @@ class TestAccount (unittest.TestCase):
         		'''
         		Function to create a user account before each test
         		'''
-        		self.new_user = User('Mary','Ng\'ang\'a','pswd100')
+        		self.new_user = User('Moha','Moha','aenshtyn@aenshtyn.com','123')
 
+
+        def test__init__(self):
+
+        		self.assertEqual(self.new_user.first_name,'Moha')
+        		self.assertEqual(self.new_user.last_name,'Moha')
+                # self.assertEqual(self.new_user.email,'aenshtyn@aenshtyn')
+        		self.assertEqual(self.new_user.password,'123')
+
+        def test_save_user(self):
+
+        		self.new_user.save_user()
+        		self.assertEqual(len(User.user_list),1)
 
 if __name__ ==  '__main__':
     unittest.main()
