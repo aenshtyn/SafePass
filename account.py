0d49e5da5438ae1f45018055e6b6db89d592cbbc
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-04-19 22:47:21 +0300

    finished syntax editing

--- account.py
+++ account.py
@@ -1,3 +1,8 @@
+import pyperclip
+import random
+import string
+
+
 global user_list
 
 class User:
@@ -24,12 +29,12 @@ class Credentials:
     credentials_list = []
     user_credentials_list = []
 	@classmethod
-	def check_user(cls,first_name,password):
+	def check_user(cls,email,password):
 
 		current_user = ''
 		for user in User.users_list:
-			if (user.first_name == first_name and user.password == password):
-				current_user = user.first_name
+			if (user.email == email and user.password == password):
+				current_user = user.email
 		return current_user
 
     def __init__(self, account_name, username, password):
