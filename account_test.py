e853713def3464701e376104f96ccb2149ff7a18
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-04-19 22:19:30 +0300

    created test to check if users

--- account_test.py
+++ account_test.py
@@ -1,13 +1,11 @@
 import unittest
+import pyperclip
 from account import User
 from account import Credentials
 
-class TestAccount (unittest.TestCase):
+class TestUser (unittest.TestCase):
 
         def setUp(self):
-        		'''
-        		Function to create a user account before each test
-        		'''
         		self.new_user = User('Moha','Moha','aenshtyn@aenshtyn.com','123')
 
 
@@ -15,13 +13,54 @@ class TestAccount (unittest.TestCase):
 
         		self.assertEqual(self.new_user.first_name,'Moha')
         		self.assertEqual(self.new_user.last_name,'Moha')
-                # self.assertEqual(self.new_user.email,'aenshtyn@aenshtyn')
+                # self.assertEqual(self.new_user.email,'aenshtyn@aenshtyn.com')
         		self.assertEqual(self.new_user.password,'123')
 
         def test_save_user(self):
 
         		self.new_user.save_user()
         		self.assertEqual(len(User.user_list),1)
+
+class TestCredentials(unittest.TestCase):
+
+        def test_check_user(self):
+
+        		self.new_user = User('Moha','Moha','aenshtyn@aenshtyn.com','123')
+        		self.new_user.save_user()
+        		user2 = User('Tom','Boy','Tom@mail.com','987')
+        		user2.save_user()
+
+        		for user in User.users_list:
+        			if user.email == user2.email and user.password == user2.password:
+        				current_user = user.first_name
+        		return current_user
+
+        		self.assertEqual(current_user,Credential.check_user(user2.password,user2.first_name))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ ==  '__main__':
     unittest.main()
