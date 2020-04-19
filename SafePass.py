b1c10af3143b92c308a8c514970727df3fd3d54b
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-04-19 13:04:35 +0300

    started creating the functionality

---
+++ SafePass.py
@@ -0,0 +1,26 @@
+def main():
+    print("Hello Welcome to your contact list. What is your name?")
+            user_name = input()
+
+            print(f"Hello {user_name}. what would you like to do?")
+            print('\n')
+
+            while True:
+                    print("Use these short codes : cc - create a new contact, dc - display contacts, fc -find a contact, ex -exit the contact list ")
+
+                    short_code = input().lower()
+
+                    if short_code == 'cc':
+                            print("New Contact")
+                            print("-"*10)
+
+                            print ("First name ....")
+                            f_name = input()
+
+                            print("Last name ...")
+                            l_name = input()
+
+                            print("Phone number ...")
+                            p_number = input()
+
+                
